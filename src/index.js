dd36d07739af58df61c914c99a19e6eb26f1b325
Fran.Maccio <franmaccio890@gmail.com>
2021-06-02 20:40:33 -0300

    Agregando boostrap al index.html y quitandolo del index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
 
     <Main />
   </React.StrictMode>,
